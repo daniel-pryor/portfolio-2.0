600887d4aa023d7f15ea232f6889ab1bdba794a1
daniel-pryor <danpryor13@gmail.com>
2023-08-23 21:42:52 +1000

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,28 @@
-const Layout = () => {
-  return <div>Layout</div>;
-};
+import { Sora } from '@next/font/google'
 
-export default Layout;
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+})
+
+// components
+
+import Nav from './Nav'
+import Header from './Header'
+import TopLeftImg from './TopLeftImg'
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sore relative`}
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  )
+}
+
+export default Layout
